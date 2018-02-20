43b1e490b77b478cf7ce265136e160cc2be32974
JerinJayaraj <36593138+JerinJayaraj@users.noreply.github.com>
2018-02-21 01:28:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ app.get('/art1', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
     
 });
-app.get('/article-two', function (req, res) {
-    res.send('Article two is requested');
+app.get('/art2', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'art2.html'));
 });
 
-app.get('/article-three', function (req, res) {
-    res.send('Article three is requested');
+app.get('/art3', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'art3.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
