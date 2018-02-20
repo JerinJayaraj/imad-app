2509c8a8098dc0ca89e047c9e4ea9278aa12167b
JerinJayaraj <36593138+JerinJayaraj@users.noreply.github.com>
2018-02-21 01:14:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/art1', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
+    
+});
 app.get('/article-two', function (req, res) {
     res.send('Article two is requested');
 });
